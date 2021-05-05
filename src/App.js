7abe83c9ecfbcf022f3cd809563e2df229457c86
kodai-kato <greatvoyage0122@gmail.com>
2021-05-05 10:21:21 +0900

    Dynamic search form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import { ApolloProvider, Query } from "react-apollo";
 import { client } from "./client";
 import { SEARCH_REPOSITORIES } from "./graphql";
 
-const VARIABLES = {
+const DEFAULT_VARIABLES = {
   first: 5,
   after: null,
   last: null,
@@ -11,9 +12,23 @@ const VARIABLES = {
 };
 
 function App() {
+  const [variables, setVariables] = useState(DEFAULT_VARIABLES);
+  const { first, after, last, before, query } = variables;
+
+  function handleChange(e) {
+    setVariables({ ...variables, query: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <ApolloProvider client={client}>
-      <Query query={SEARCH_REPOSITORIES} variables={VARIABLES}>
+      <form onSubmit={handleSubmit}>
+        <input value={query} onChange={handleChange} />
+      </form>
+      <Query query={SEARCH_REPOSITORIES} variables={variables}>
         {({ loading, error, data }) => {
           if (loading) return "Loading...";
           if (error) return `Error! ${error.message}`;
